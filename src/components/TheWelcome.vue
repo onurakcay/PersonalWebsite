a4f072a5634a527954cdca87f8ef7b4e249eaf1f
Onur AKCAY <onurakcay.tr@gmail.com>
2022-11-11 21:33:05 +0300

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="helloText">{{ msg }}</h1>
+    <v-avatar image="@/assets/profile.png" size="66"></v-avatar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+.greetings {
+  width: 100%;
+  h1,
+  h3 {
+    text-align: center;
+  }
+}
+.helloText {
+  font-size: 48px;
+  font-weight: bold;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <header>
+      <div class="wrapper">
+        <TheWelcome msg="Merhaba"/>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
